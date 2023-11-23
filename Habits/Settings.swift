c2cca39a31a909eb87fc1189562282f5bd2147a9
Tatsuya Moriguchi <tatsuya.moriguchi@icloud.com>
2023-11-23 13:09:49 -0800

    Set up the Layout and Data Source

--- Habits/Settings.swift
+++ Habits/Settings.swift
@@ -68,5 +68,18 @@ struct Settings {
         }
     }
     
+    // Toggle the status of a user.
+    mutating func toggleFollowed(user: User) {
+        var updated = followedUserIDs
+        
+        if updated.contains(user.id) {
+            updated = updated.filter({ $0 != user.id })
+        } else {
+            updated.append(user.id)
+        }
+        
+        followedUserIDs = updated
+    }
+    
 }
 
--- Habits/UserCollectionViewController.swift
+++ Habits/UserCollectionViewController.swift
@@ -4,33 +4,16 @@
 //
 //  Created by Tatsuya Moriguchi on 11/20/23.
 //
-
 import UIKit
-
-private let reuseIdentifier = "Cell"
-
+private let reuseIdentifier = "User"
 class UserCollectionViewController: UICollectionViewController {
 
     // Cancel the task at the top of the class.
     var usersRequestTask: Task<Void, Never>? = nil
     deinit { usersRequestTask?.cancel() }
-
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Register cell classes
-        self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
-
-        // Do any additional setup after loading the view.
-    }
-    
-//    While you're there, typealias your data source type, create your model type, and add declarations for the data source and model properties.
+    //    While you're there, typealias your data source type, create your model type, and add declarations for the data source and model properties.
     typealias DataSourceType = UICollectionViewDiffableDataSource<ViewModel.Section,ViewModel.Item>
-    
+
     enum ViewModel {
         typealias Section = Int
         
@@ -47,7 +30,7 @@ class UserCollectionViewController: UICollectionViewController {
             }
         }
     }
-    
+
     struct Model {
         var usersByID = [String:User]()
         var followedUsers: [User] {
@@ -56,14 +39,64 @@ class UserCollectionViewController: UICollectionViewController {
             }.values)
         }
     }
-    
+
     var dataSource: DataSourceType!
     var model = Model()
+    //    While you could also just typealias Item to User, you're not maintaining a separate section for followed users in this screen as you did for habits. If you might later want to visually distinguish between followed and unfollowed users, you should include that information in the view model as part of the Item.
+    //    Note that you explicitly implemented hash(into:) and ==(_:_:) for the Item struct. (The free implementations of those two methods would have used both properties of Item in their calculations.) You want to make sure that, from the perspective of the collection view's data source, the identity of a user doesn't change when their followed status changes. If it did, your collection view would unnecessarily remove and add a cell.
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Register cell classes
+        self.collectionView!.register(UICollectionViewListCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+
+        dataSource = createDataSource()
+        collectionView.dataSource = dataSource
+        collectionView.collectionViewLayout = createLayout()
+        
+        // Get the users from the API service.
+        update()
+        
+    }
+
+    // Dequeue and configure a cell with a user's name
+    func createDataSource() -> DataSourceType {
+        let dataSource = DataSourceType(collectionView: collectionView) {
+            (collectionView, indexPath, item) in
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! UICollectionViewListCell
+            
+            var content = cell.defaultContentConfiguration()
+            content.text = item.user.name
+            // make some adjustments to the contentConfiguration to adjust the directionalLayoutMargins and the alignment of the textProperty so that the user's name is centered in the cell.
+            content.directionalLayoutMargins = NSDirectionalEdgeInsets(top: 11, leading: 8, bottom: 11, trailing: 8)
+            content.textProperties.alignment = .center
+            cell.contentConfiguration = content
+            
+            return cell
+            
+        }
+        
+        return dataSource
+    }
     
-//    While you could also just typealias Item to User, you're not maintaining a separate section for followed users in this screen as you did for habits. If you might later want to visually distinguish between followed and unfollowed users, you should include that information in the view model as part of the Item.
-//    Note that you explicitly implemented hash(into:) and ==(_:_:) for the Item struct. (The free implementations of those two methods would have used both properties of Item in their calculations.) You want to make sure that, from the perspective of the collection view's data source, the identity of a user doesn't change when their followed status changes. If it did, your collection view would unnecessarily remove and add a cell.
-    
-    
+    // Implements a grid-style interface with two columns of square cells
+    func createLayout() -> UICollectionViewCompositionalLayout {
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalHeight(1), heightDimension: .fractionalHeight(1))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalWidth(0.45))
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitem: item, count: 2)
+        group.interItemSpacing = .fixed(20)
+        
+        let section = NSCollectionLayoutSection(group: group)
+        section.interGroupSpacing = 20
+        section.contentInsets = NSDirectionalEdgeInsets(top: 20, leading: 20, bottom: 20, trailing: 20)
+        
+        return UICollectionViewCompositionalLayout(section: section)
+    }
+
+
     // To fetch users
     func update() {
         usersRequestTask?.cancel()
@@ -89,6 +122,21 @@ class UserCollectionViewController: UICollectionViewController {
         
         dataSource.applySnapshotUsing(sectionIDs: [0], itemsBySection: itemsBySection)
     }
-
+    
+    
+    
+    func collectionView(_ collectionView: UICollectionView, contextMenuConfigurationForItemsAt indexPath: IndexPath, point: CGPoint) -> UIContextMenuConfiguration? {
+        let config = UIContextMenuConfiguration(identifier: nil, previewProvider: nil) { (elements) -> UIMenu? in
+            
+            guard let item = self.dataSource.itemIdentifier(for: indexPath) else { return nil }
+            
+            let favoriteToggle = UIAction(title: item.isFollowed ? "Unfollow" : "Follow") { (action) in
+                Settings.shared.toggleFollowed(user: item.user)
+                self.updateCollectionView()
+            }
+            return UIMenu(title: "", image: nil, identifier: nil, options: [], children: [favoriteToggle])
+        }
+        return config
+    }
     
 }
