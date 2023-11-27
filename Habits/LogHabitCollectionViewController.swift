15a194bdab232153895f58ff22a7a864420b9674
Tatsuya Moriguchi <tatsuya.moriguchi@icloud.com>
2023-11-26 18:43:19 -0800

    Log Habits

--- Habits/LogHabitCollectionViewController.swift
+++ Habits/LogHabitCollectionViewController.swift
@@ -46,6 +46,24 @@ class LogHabitCollectionViewController: HabitCollectionViewController {
                 return section
             }
         }
-    }   
+        
+        
+        // Notice that a reference to the task is not saved in this case, as there is no reason to try to cancel it.
+        // All requests to send the data to the server should continue to be tried until it succeeds or times out.
+        // (Of course, in a real-world example you would want to handle errors, so the user has the opportunity to submit their request again.) A new Task will be created each time a habit is logged.
+        func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+            guard let item = dataSource.itemIdentifier(for: indexPath)
+               else { return }
+
+            let loggedHabit = LoggedHabit(userID:
+               Settings.shared.currentUser.id, habitName: item.name,
+               timestamp: Date())
+
+            Task {
+                try? await LogHabitRequest(loggedHabit: loggedHabit).send()
+            }
+        }
+
+    }
     
 }
