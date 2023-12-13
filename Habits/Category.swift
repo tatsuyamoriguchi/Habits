ddfd18802f550a6c6db1f393e94352b0ad3a39d8
Tatsuya Moriguchi <tatsuya.moriguchi@icloud.com>
2023-12-12 21:58:31 -0800

    struct Color was misplaced in Habit.swift, instead of creating Color.swift. Added two more Color extensions

--- Habits/Category.swift
+++ Habits/Category.swift
@@ -15,19 +15,6 @@ struct Category {
 
 extension Category: Codable { }
 
-struct Color {
-    let hue: Double
-    let saturation: Double
-    let brightness: Double
-}
-
-extension Color: Codable {
-    enum CodingKeys: String, CodingKey {
-        case hue = "h"
-        case saturation = "s"
-        case brightness = "b"
-    }
-}
 
 extension Category: Hashable {
     func hash(into hasher: inout Hasher) {
---
+++ Habits/Color.swift
@@ -0,0 +1,30 @@
+//
+//  Color.swift
+//  Habits
+//
+//  Created by Tatsuya Moriguchi on 12/12/23.
+//
+
+import UIKit
+
+struct Color {
+    let hue: Double
+    let saturation: Double
+    let brightness: Double
+}
+
+extension Color: Codable {
+    enum CodingKeys: String, CodingKey {
+        case hue = "h"
+        case saturation = "s"
+        case brightness = "b"
+    }
+}
+
+extension Color {
+    var uiColor: UIColor {
+        return UIColor(hue: CGFloat(hue), saturation: CGFloat(saturation), brightness: CGFloat(brightness), alpha: 1)
+    }
+}
+
+extension Color: Hashable { }
